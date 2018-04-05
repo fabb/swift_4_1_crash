545dcbdf780d27d8c6d9e4b1274478ed2166f35f
Fabian Ehrentraud <fabian.ehrentraud@willhaben.at>
2018-04-05 13:51:43 +0200

    added crashing code

--- swift_4_1_crash/AppDelegate.swift
+++ swift_4_1_crash/AppDelegate.swift
@@ -1,5 +1,41 @@
 import UIKit
 
+
+// crashes when called on an optimized build (-O or -Osize)
+// this exact repro only crashes on the simulator, but there are other repros that also crash on the device
+func crash() {
+	_ = ProblematicEnum.problematicCase.problematicMethod()
+}
+
+enum ProblematicEnum {
+	case first, second, problematicCase
+	
+	func problematicMethod() -> SomeClass {
+		let someVariable: SomeClass
+		
+		switch self {
+		case .first:
+			someVariable = SomeClass()
+		case .second:
+			someVariable = SomeClass()
+		case .problematicCase:
+			someVariable = SomeClass(someParameter: NSObject())
+			_ = NSObject().description
+			return someVariable // EXC_BAD_ACCESS (simulator: EXC_I386_GPFLT)
+		}
+		
+		let _ = [someVariable]
+		return SomeClass(someParameter: NSObject())
+	}
+	
+}
+
+class SomeClass: NSObject {
+	override init() {}
+	init(someParameter: NSObject) {}
+}
+
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -8,6 +44,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
 
+		crash()
 
 		return true
 	}
